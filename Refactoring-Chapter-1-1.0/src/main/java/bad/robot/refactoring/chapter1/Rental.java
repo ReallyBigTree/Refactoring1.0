e06fc66cabc7d9003839a8298a5f572aba3d108a
W <W@DESKTOP-OG1EDTQ>
2022-02-03 01:33:52 -0500

    First commit, includes refactoring changes

---
+++ Refactoring-Chapter-1-1.0/src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -0,0 +1,52 @@
+package main.java.bad.robot.refactoring.chapter1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Rental {
+
+    private Movie movie;
+    private int daysRented;
+
+    public Rental(Movie movie, int daysRented) {
+        this.movie = movie;
+        this.daysRented = daysRented;
+    }
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public int getDaysRented() {
+        return daysRented;
+    }
+
+	double getChargeFor(Rental rental) {
+		double amount = 0;
+		switch (rental.getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		        amount += 2;
+		        if (rental.getDaysRented() > 2)
+		            amount += (rental.getDaysRented() - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        amount += rental.getDaysRented() * 3;
+		        break;
+		    case Movie.CHILDREN:
+		        amount += 1.5;
+		        if (rental.getDaysRented() > 3)
+		            amount += (rental.getDaysRented() - 3) * 1.5;
+		        break;
+		}
+		return amount;
+	}
+	int calculateFrequentRenterPoints(int frequentRenterPoints, Rental rental) {
+		
+		frequentRenterPoints++;
+	
+		if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+		    frequentRenterPoints++;
+		return frequentRenterPoints;
+	}
+
+}
